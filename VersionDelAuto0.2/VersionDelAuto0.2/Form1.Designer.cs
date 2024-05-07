bbd817f87fd25072b2376d915d5c592c00989ae2
Anthony Tolentino <141686020+Tolentino995@users.noreply.github.com>
2024-05-07 17:15:15 -0300

    0.02

---
+++ VersionDelAuto0.2/VersionDelAuto0.2/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace VersionDelAuto0._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            Jugador = new PictureBox();
+            lblPuntos = new Label();
+            lblVidas = new Label();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Jugador).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.Autos_encontra_del_usuaruio;
+            pictureBox1.Location = new Point(242, 31);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(90, 122);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Tag = "Obstaculos";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Cursor = Cursors.No;
+            pictureBox2.Image = Properties.Resources.Piedra_;
+            pictureBox2.Location = new Point(125, 74);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(51, 45);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Tag = "Obstaculos";
+            // 
+            // Jugador
+            // 
+            Jugador.BackColor = Color.Transparent;
+            Jugador.Image = Properties.Resources.Auto_de_usuari;
+            Jugador.Location = new Point(177, 377);
+            Jugador.Name = "Jugador";
+            Jugador.Size = new Size(85, 102);
+            Jugador.SizeMode = PictureBoxSizeMode.StretchImage;
+            Jugador.TabIndex = 2;
+            Jugador.TabStop = false;
+            // 
+            // lblPuntos
+            // 
+            lblPuntos.AutoSize = true;
+            lblPuntos.Font = new Font("Segoe UI Historic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPuntos.Location = new Point(395, 9);
+            lblPuntos.Name = "lblPuntos";
+            lblPuntos.Size = new Size(83, 21);
+            lblPuntos.TabIndex = 3;
+            lblPuntos.Text = "Puntos: 0";
+            // 
+            // lblVidas
+            // 
+            lblVidas.AutoSize = true;
+            lblVidas.Font = new Font("Segoe UI Historic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblVidas.Location = new Point(395, 50);
+            lblVidas.Name = "lblVidas";
+            lblVidas.Size = new Size(72, 21);
+            lblVidas.TabIndex = 4;
+            lblVidas.Text = "Vidas: 3";
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += MainGameTimer;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.PistaDeCarrera0_1;
+            ClientSize = new Size(490, 511);
+            Controls.Add(lblVidas);
+            Controls.Add(lblPuntos);
+            Controls.Add(Jugador);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            KeyDown += KeyIsDown;
+            KeyUp += KeysIsUp;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Jugador).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox Jugador;
+        private Label lblPuntos;
+        private Label lblVidas;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
---
+++ VersionDelAuto0.2/VersionDelAuto0.2/Form1.cs
@@ -0,0 +1,101 @@
+namespace VersionDelAuto0._2
+{
+    public partial class Form1 : Form
+    {
+
+        bool uLeft, uRight;
+
+        int speed = 8;
+        int puntos = 0;
+        int vidas = 0;
+
+        Random ranX = new Random();
+        Random ranY = new Random();
+
+        PictureBox Moneda = new PictureBox();
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void MainGameTimer(object sender, EventArgs e)
+        {
+
+            lblVidas.Text = "Vidas: " + vidas;
+            lblPuntos.Text = "Puntos: " + puntos;
+
+            if (uLeft == true && Jugador.Left > 0)
+            {
+                Jugador.Left -= 1;
+            }
+            if (uRight == true && Jugador.Left + Jugador.Width < this.ClientSize.Width)
+            {
+                Jugador.Left += 1;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "Obstaculos")
+                {
+                    x.Top += speed;
+                }
+
+                if (x.Top + x.Height > this.ClientSize.Height)
+                {
+                    x.Top = ranY.Next(80, 300) * -1;
+                    x.Left = ranX.Next(5, this.ClientSize.Width - x.Width);
+                }
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) 
+            { 
+                uLeft = true;
+            }
+            if (e.KeyCode == Keys.Right) 
+            {
+                uRight = true;
+            }
+        }
+
+        private void KeysIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                uLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                uRight = false;
+            }
+        }
+
+        private void RestartGame()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "Obstaculos")
+                {
+                    x.Top = ranY.Next(80, 300) * -1;
+                    x.Left = ranX.Next(5, this.ClientSize.Width - x.Width);
+                }
+            }
+
+            Jugador.Left = this.ClientSize.Width / 2;
+            Jugador.Image = Properties.Resources.Auto_de_usuari;
+
+            puntos = 0;
+            vidas = 3;
+            speed = 8;
+
+            uLeft = false;
+            uRight = false;
+
+            GameTimer.Start();
+
+        }
+    }
+}
